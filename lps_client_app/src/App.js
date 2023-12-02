5387d801ca279a292340a2fa9419c5d4b0951bd0
Joshua Duplaa <joshuaduplaa@outlook.com>
2023-12-02 16:29:38 -0600

    added pages

--- lps_client_app/src/App.js
+++ lps_client_app/src/App.js
@@ -2,6 +2,11 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import WinningCards from './components/WinningCards';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Tickets from './pages/browsetickets';
+import AboutUs from './pages/aboutus';
+import WinningTickets from './pages/winningTickets';
+import Profile from './pages/profile';
 
 function App() {
   return (
