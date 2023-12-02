ecda57dde3cd5479a938f60d57510b4b0b53f6d8
Joshua Duplaa <joshuaduplaa@outlook.com>
2023-12-02 16:40:54 -0600

    Pages are now accessible

--- lps_client_app/src/App.js
+++ lps_client_app/src/App.js
@@ -1,20 +1,43 @@
 // App.js
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from './components/Navbar';
 import WinningCards from './components/WinningCards';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 import Tickets from './pages/browsetickets';
 import AboutUs from './pages/aboutus';
 import WinningTickets from './pages/winningTickets';
 import Profile from './pages/profile';
+import LoginPage from './pages/login'; // assuming you have a LoginPage component
 
 function App() {
-  return (
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const handleLogin = () => {
+    // Implement your login logic here
+    setIsAuthenticated(true);
+  };
+
+  const handleLogout = () => {
+    // Implement your logout logic here
+    setIsAuthenticated(false);
+  };
+
+  return (    
     <div className="App">
-      <Navbar />
+      <Router>
+      <Navbar isAuthenticated={isAuthenticated} onLogout={handleLogout} />
       <main>
-        <WinningCards />
+        <Routes>
+          <Route path="/" element={<WinningCards />} />
+          <Route path="/tickets" element={<Tickets />} />
+          <Route path="/about-us" element={<AboutUs />} />
+          <Route path="/winnings" element={<WinningTickets />} />
+          <Route path="/profile" element={isAuthenticated ? <Profile /> : <Navigate to="/login" />} />
+          <Route path="/login" element={!isAuthenticated ? <LoginPage onLogin={handleLogin} /> : <Navigate to="/profile" />} />
+        </Routes>
       </main>
+    </Router>
+    
     </div>
   );
 }
