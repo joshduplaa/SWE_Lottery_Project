15cfc5af97ef284857b832187eed16a42b6202e7
Joshua Duplaa <joshuaduplaa@outlook.com>
2023-12-02 22:11:18 -0600

    fixed navbar

--- lps_client_app/src/components/Navbar.js
+++ lps_client_app/src/components/Navbar.js
@@ -3,11 +3,12 @@ import React from 'react';
 function Navbar() {
   return (
     <nav>
-      <div className="profile">Profile</div>
+      <div className="title">Texas Lottery Purchase system</div>
       <div className="nav-links">
         <a href="/tickets">Tickets</a>
         <a href="/about-us">About Us</a>
         <a href="/winnings">Winnings</a>
+        <a href="/profile">Profile</a>
       </div>
     </nav>
   );
